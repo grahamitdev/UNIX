afc8d1d792a1c1090cf2f2685cc9191fd4cd65a5
wwwwwalter <gzyiot2017@163.com>
2018-04-18 00:10:27 +0800

    dup2

--- packages/lseek/dup.c
+++ packages/lseek/dup.c
@@ -4,19 +4,31 @@
 
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <pthread.h>
+
+void *dup_thread(void *p)
+{
+    while(1)
+    {
+        printf("helloworld\n");
+    }
+}
 
 int main(void)
 {
     int fd = 0;
-    fd = open("file",O_RDWR);
-    printf("%d\n",fd);
-    int fd_out = dup(1);
-    dup2(fd,1);
-    printf("helloworld\n");
-    dup2(fd_out,1);
+    int fd_out = 0;
+    fd = open("file", O_RDWR);
+    fd_out = dup(1);
+    dup2(fd, 1);
+    
+
+    pthread_t pid;
+    pthread_create(&pid, NULL, dup_thread, NULL);
+    usleep(5);
+    dup2(fd_out, 1);
     printf("hello\n");
-
-
+    
 
     return 0;
 }
